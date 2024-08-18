0f235a2b514d5d87962d3614274b75a955b5ef8e
Rory Stephenson <rorystephenson@gmail.com>
2024-08-18 23:46:04 +0200

    Split paragliding and deltaplane quizzes

--- generate-deck.rb
+++ generate-deck.rb
@@ -1,18 +1,21 @@
 require 'csv'
 require 'anki_record'
 
-OUTPUT_NAME = 'vds-quiz'
-OUTPUT_FILE = 'vds-quiz.apkg'
+PARAGLIDE_OUTPUT_NAME = "vds-quiz-parapendio"
+DELTAPLANE_OUTPUT_NAME = "vds-quiz-deltaplano"
+PARAGLIDE_OUTPUT_FILE = "#{PARAGLIDE_OUTPUT_NAME}.apkg"
+DELTAPLANE_OUTPUT_FILE = "#{DELTAPLANE_OUTPUT_NAME}.apkg"
 
 def extract(question_with_answer)
   answers = [
-    question_with_answer['answer_1'].capitalize,
-    question_with_answer['answer_2'].capitalize,
-    question_with_answer['answer_3'].capitalize,
-  ]
+    question_with_answer['answer_1'],
+    question_with_answer['answer_2'],
+    question_with_answer['answer_3'],
+  ].each { |a| a[0] = a[0].upcase }
+
   solution_index = question_with_answer['solution'].to_i - 1
   correct_answer = answers.delete_at solution_index
-  
+
   {
     id: question_with_answer['id'],
     question: question_with_answer['question'],
@@ -21,11 +24,57 @@ def extract(question_with_answer)
   }
 end
 
-File.delete(OUTPUT_FILE) if File.exist?(OUTPUT_FILE)
+def create_deck(file_name, deck_name, css, question_format, answer_format, questions)
+  AnkiRecord::AnkiPackage.create(name: file_name) do |anki21_database|
+    # Creating a new deck
+    deck = AnkiRecord::Deck.new(anki21_database:, name: deck_name)
+    deck.save
+  
+    # Creating a new note type
+    note_type = AnkiRecord::NoteType.new(
+      anki21_database:,
+      name: "question",
+    )
+    note_type.css = css
+  
+    AnkiRecord::NoteField.new(note_type: note_type, name: "Id")
+    AnkiRecord::NoteField.new(note_type: note_type, name: "Question")
+    AnkiRecord::NoteField.new(note_type: note_type, name: "Choices")
+  
+    card_template = AnkiRecord::CardTemplate.new(note_type: note_type, name: "multi choice")
+    card_template.question_format = question_format
+    card_template.answer_format = answer_format
+  
+    note_type.save
+  
+    # Creating notes
+    for question in questions do
+      note = AnkiRecord::Note.new(note_type: note_type, deck: deck)
+      note.id = question[:id]
+      note.question = question[:question]
+      note.choices = question[:other_answers].prepend(question[:correct_answer]).join("<br>")
+      note.save
+    end
+  end
+end
+
+File.delete(PARAGLIDE_OUTPUT_FILE) if File.exist?(PARAGLIDE_OUTPUT_FILE)
+File.delete(DELTAPLANE_OUTPUT_FILE) if File.exist?(DELTAPLANE_OUTPUT_FILE)
 
 # Read question/answer CSV
-questions_with_answers = CSV.open('vds-questions-with-answers.csv', headers: :first_row).map(&:to_h)
-questions_with_answers = questions_with_answers.map { |q| extract(q) }
+questions = CSV.open('vds-questions-with-answers.csv', headers: :first_row).map(&:to_h).map{ |q| extract(q) }
+
+# Split to:
+# - paraglider with general questions
+# - deltaplane with general questions
+paraglide_questions = questions.select do |q|
+  question = q[:question].downcase
+  !question.include?("deltaplan") || question.include?("parapend")
+end
+deltaplane_questions = questions.select do |q|
+  question = q[:question].downcase
+  !question.include?("parapend") || question.include?("deltaplan")
+end
 
 # Read anki templates
 front_html = IO.read(File.join('anki-templates', 'front.html'))
@@ -34,46 +83,10 @@ front_js = IO.read(File.join('anki-templates', 'front.js'))
 back_js = IO.read(File.join('anki-templates', 'back.js'))
 css = IO.read(File.join('anki-templates', 'card.css'))
 
-AnkiRecord::AnkiPackage.create(name: "vds-quiz") do |anki21_database|
-  # Creating a new deck
-  deck = AnkiRecord::Deck.new(anki21_database:, name: "VDS Quiz")
-  deck.save
+question = "<b>{{Id}}.</b> {{Question}}<br><br>"
+question_format = "#{question}#{front_html}\n<script>#{front_js}</script>"
+answer_format = "#{question}#{back_html}\n<script>#{back_js}</script>"
 
-  # Creating a new note type
-  note_type = AnkiRecord::NoteType.new(
-    anki21_database:,
-    name: "question",
-  )
-  note_type.css = css
+create_deck(PARAGLIDE_OUTPUT_NAME, "VDS Quiz Parapendio", css, question_format, answer_format, paraglide_questions)
+create_deck(DELTAPLANE_OUTPUT_NAME, "VDS Quiz Deltaplano", css, question_format, answer_format, deltaplane_questions)
 
-  AnkiRecord::NoteField.new(
-    note_type: note_type,
-    name: "Id",
-  )
-  AnkiRecord::NoteField.new(
-    note_type: note_type,
-    name: "Question",
-  )
-  AnkiRecord::NoteField.new(
-    note_type: note_type,
-    name: "Choices",
-  )
-  card_template = AnkiRecord::CardTemplate.new(
-    note_type: note_type,
-    name: "multi choice"
-  )
-  question = "<b>{{Id}}.</b> {{Question}}<br><br>"
-  card_template.question_format = "#{question}#{front_html}\n<script>#{front_js}</script>"
-  card_template.answer_format = "#{question}#{back_html}\n<script>#{back_js}</script>"
-
-  note_type.save
-
-  # Creating notes
-  for question_with_answer in questions_with_answers do
-    note = AnkiRecord::Note.new(note_type: note_type, deck: deck)
-    note.id = question_with_answer[:id]
-    note.question = question_with_answer[:question]
-    note.choices = question_with_answer[:other_answers].prepend(question_with_answer[:correct_answer]).join("<br>")
-    note.save
-  end
-end
